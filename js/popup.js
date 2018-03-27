9bdd6885ca94083e4a0d7c385353cac4580b26df
Wim Nijmeijer <wnijmeijer@coveo.com>
2018-03-27 12:32:25 +0200

    Added CopyToClipboard

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,46 @@
 // jshint -W110, -W003
 /*global chrome, createWheel*/
 
+
+//CopyToClipboard so we can copy/paste the part of the report
+function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text); 
+
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
+//Copy the report
+function copyReport(id){
+    try {
+        var html = document.getElementById(id).innerHTML;
+        //TODO: remove the copy buttons from the HTML?
+        var successful = copyToClipboard(html);
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying Report to Clipboard was ' + msg);
+        alert("Report copied to clipboard.")
+      } catch (err) {
+        console.log('Oops, unable to copy to Clipboard');
+        alert("Report WAS NOT copied to clipboard.")
+    }
+}
+
+
 let processDetail = (section, data, tests) => {
   let lines = section.attributes.map(attr => {
 
